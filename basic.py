64e3e4953c8c7431f5972c9f1ce1935fa578f016
dylan <forde.dylan@gmail.com>
2025-08-29 21:33:51 +0100

    jit and autodiff

--- basic.py
+++ basic.py
@@ -4,6 +4,7 @@
 
 # JAX as NumPy
 import jax
+from jax._src.config import _validate_default_device
 import jax.numpy as jnp
 import time
 
@@ -45,9 +46,76 @@ myfunction(arr)
 end = time.perf_counter()
 print(f"not blocked until ready: {end - start}")
 
+# intermediate representation that jax uses
+# print(jax.make_jaxpr(myfunction)(arr))
+
+# not possible to do
+# @jax.jit
+# def f(x):
+#     if x % 2 == 0:
+#         return 1
+#     else:
+#         return 0
+
+# f(10)
 
 # Automatic Differentiation
+def square(x):
+    return x ** 2
 
+# needs to be a float
+value = 10.0
+print('automatic differentiation square function')
+print(square(value))
+
+print(jax.grad(square)(value))
+print(jax.grad(jax.grad(square))(value))
+print(jax.grad(jax.grad(jax.grad(square)))(value))
+
+
+def f(x, y, z):
+    return x ** 2 + 2 * y ** 2 + 3 * z ** 2
+
+x, y, z = 2.0, 2.0, 2.0
+print('automatic differentiation polynomial')
+print(f(x, y, z))
+print(jax.grad(f, argnums=0)(x, y, z))
+print(jax.grad(f, argnums=1)(x, y, z))
+print(jax.grad(f, argnums=2)(x, y, z))
+
+def f_arr(arr):
+     return arr[0] ** 2 + 2 * arr[1] ** 2 + 3 * arr[2] ** 2
+
+print('automatic differentiation with list rather than separate values')
+print(f_arr([x, y, z]))
+print(jax.grad(f_arr)([x, y, z]))
+ 
 # Automatic Vectorization
+
+key = jax.random.key(42)
+
+W = jax.random.normal(key, (150, 100)) # 100 values per input sample, 150 neurons in next layer
+X = jax.random.normal(key, (10, 100))
+
+def calculate_output(x):
+    return jnp.dot(W, x)
+
+# not the most efficient way
+def batched_calculation_la(X):
+    return jnp.dot(X, W.T)
+
+batched_calculation_vmap = jax.vmap(calculate_output)
+
+start = time.perf_counter()
+batched_calculation_la(X)
+end = time.perf_counter()
+print(f'la batch calculation: {end-start}')
+
+start = time.perf_counter()
+batched_calculation_vmap(X)
+end = time.perf_counter()
+print(f'vmap batch calculation: {end-start}')
+
+print(jnp.allclose(batched_calculation_vmap(X), batched_calculation_la(X), atol=1E-4, rtol=1E-4))
 
 # Randomness
