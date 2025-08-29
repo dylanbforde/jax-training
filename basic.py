fe000884837491e6ad48d6a7dd367cace9fe38f0
dylan <forde.dylan@gmail.com>
2025-08-29 19:25:44 +0100

    format

--- basic.py
+++ basic.py
@@ -5,6 +5,7 @@
 # JAX as NumPy
 import jax
 import jax.numpy as jnp
+import time
 
 a = jnp.array([1, 2, 3])
 b = jnp.array([4, 5, 6])
@@ -21,28 +22,28 @@ print(b.reshape(-1, 1))
 a = a.at[1].set(10)
 
 # JIT Compilation
-import time
 
 @jax.jit
 def myfunction(x):
-    return jnp.where(x % 2 == 0, x / 2, 3 * x + 1) # Collatz Conjecture
+    return jnp.where(x % 2 == 0, x / 2, 3 * x + 1)  # Collatz Conjecture
+
 
 arr = jnp.arange(10)
 
-_ = myfunction(arr) # warm up for jit
+_ = myfunction(arr)  # warm up for jit
 
 # this blocks the end line from running until the value is computed
 start = time.perf_counter()
 myfunction(arr).block_until_ready()
 end = time.perf_counter()
-print(f'block until ready: {end-start}')
+print(f"block until ready: {end - start}")
 
 # this does not block the end line from running until the value is computed
 # essentially the next python line is ran before the value is computed
 start = time.perf_counter()
 myfunction(arr)
 end = time.perf_counter()
-print(f'not blocked until ready: {end-start}')
+print(f"not blocked until ready: {end - start}")
 
 
 # Automatic Differentiation
